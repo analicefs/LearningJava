72426f79336e0fdffa6da894b8d3912cd92efed2
Ana Lice <analice87@hotmail.com>
2020-05-25 20:59:43 -0300

    Adicionei uma linha no código

--- AulaJava/src/System/out/Conta.java
+++ AulaJava/src/System/out/Conta.java
@@ -2,29 +2,34 @@ package System.out;
 
 public class Conta {
 	private String numero;
-	 public String getNumero() {
-		return numero;
-	}
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-	public double getSaldo() {
+	private double saldo;
+	
+public  Conta(){
+	this.numero=numero;
+}
+public String getNumero() {
+	return this.numero;
+}
+	
+public double getSaldo() {
 		return saldo;
 	}
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-	private double saldo;
-	 
-	public Conta(String numero) {
+	
+public Conta(String numero) {
 		this.numero=numero;
 	}
-	public void Creditar(double valor) {
+public void Creditar(double valor) {
 		this.saldo+=valor;
 	}
-	public void Debitar(double valor) {
+public void Debitar(double valor) {
 		this.saldo= saldo-valor;
 	
+	}
+public static void main(String[] args) {
+		Conta p= new Conta();
+
+		
+		System.out.println("");
 	}
 
 }
